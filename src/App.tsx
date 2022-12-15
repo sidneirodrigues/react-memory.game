399da2e0f92d53fc70b4ef86d953692fc220db40
Sidnei Rodrigues <sidnei.c.rodrigues@hotmail.com>
2022-12-14 21:27:30 -0300

    criado componente InfoItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+import * as C from './App.styles';
 
+//Img
+import logoImage from './assets/devmemory_logo.png';
+// import restartIcon from './assets/svgs/restart.svg';
 
-function App() {
+//Components
+import { InfoItem } from './components/InfoItem';
 
+//Functions
+const resetAndCreateGrid = () => {
+    
+}
+ 
+const App = () => {
   return (
-    <div>
-      Wello word
-    </div>
+    <C.Container>
+        <C.Info>
+            <C.LogoLink href="">
+                <img src={logoImage} width="200" alt="" />
+            </C.LogoLink>
+
+            <C.InfoArea>
+                <InfoItem label='Tempo' value='00:00'/>
+                <InfoItem label='Movimentos' value='0' />
+            </C.InfoArea>
+
+            
+        </C.Info>
+        <C.GridArea>
+            ...
+        </C.GridArea>
+    </C.Container>
+
   )
 }
 
-export default App
+
+export default App;
---
+++ src/components/InfoItem/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as C from './styles'
+
+type Props = {
+    label: string;
+    value: string;
+}
+
+export function InfoItem({ label, value }: Props) {
+  return (
+    <C.Container>
+        <C.Label>{ label }</C.Label>
+        <C.Value>{ value }</C.Value>
+    </C.Container>
+  )
+}
+
