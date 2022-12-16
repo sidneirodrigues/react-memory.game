d98979e59c7a2349d4cfac7e890ac85cda2129c2
Sidnei Rodrigues <sidnei.c.rodrigues@hotmail.com>
2022-12-16 00:15:40 -0300

    estilizando o grid

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -11,12 +11,22 @@ type Props = {
 
 export const GridItem = ({item, onClick}: Props) => {
   return (
-    <C.Container onClick={onClick}>
+    <C.Container 
+        showBackGround={ item.permanentShown || item.shown }
+        onClick={onClick}        
+    >
         { !item.permanentShown && !item.shown &&
-            <C.Icon src={b7Svg} alt="" />
+            <C.Icon 
+                src={b7Svg} 
+                alt="" 
+                opacity={.1}
+            />
         }
         { ( item.permanentShown || item.shown ) && item.item !== null &&
-            <C.Icon src={items[item.item].icon} alt="" />
+            <C.Icon 
+                src={items[item.item].icon} 
+                alt="" 
+            />
         }
     </C.Container>
   )
